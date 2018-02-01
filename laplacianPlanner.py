8dac5bfcedd7d97b6f9bea5ddb52302b741ff7d1
Suvo <subhabrata.ganguli@honeywell.com>
2018-02-01 15:34:24 -0500

    working laplacian.py

--- laplacianPlanner.py
+++ laplacianPlanner.py
@@ -32,29 +32,34 @@ def laplacian( start_point, end_point, nxs, nys, nzs, nzs_low, obstacles, slow_c
             n_vec_exponents = [5, 4, 5, 4, 5, 6, 6]
             iter_max = 50
 
-    n_vec = 2.0 ** np.array(n_vec_exponents)
+    n_vec = 2 ** np.array(n_vec_exponents)
     ny_factor = nxs / nys # POWER OF 2     (y grid_size) = (x grid size)/ny_factor
     nz_factor = nxs / nzs # POWER OF 2     (z grid_size) = (x grid size)/nz_factor
 
-    dt = float(nxs) / 128 # number of pixels per time step
+    #dt = float(nxs) / 128 # number of pixels per time step
+    dt = 4
 
     # we want to make nt large enough for the path to run across the entire domain
     i_n = max(n_vec_exponents)
     nx_ = 2 ** i_n
     ny_ = nx_ / ny_factor
     nz_ = nx_ / nz_factor
-    nt = round(np.linalg.norm(np.array([nx_, ny_, nz_]) / dt))
-
-
+    nt = int(np.linalg.norm(np.array([nx_, ny_, nz_]) / dt))
+    nz_low = int(nz_/2)
 
     # ----------------  Build obstacles ----------------
 
 
-    for i_n in np.arange(min(n_vec_exponents), max(n_vec_exponents)):
+    for i_n in np.arange(min(n_vec_exponents), max(n_vec_exponents)+1):
         nx = 2 ** i_n
         ny = nx / ny_factor # this needs to be updated
         nz = nx / nz_factor
         nz_low = nz * (nzs_low/ nzs)
+
+        # nx = int(nx)
+        # ny = int(ny)
+        # nz = int(nz)
+        # nz_low = int(nz_low)
 
         # Assemble obstacles
         obstacle = np.zeros([nx, ny, nz]) # obstacle array has logicals, (-1,0,1) for (goal, interior, obstacle)
@@ -86,33 +91,36 @@ def laplacian( start_point, end_point, nxs, nys, nzs, nzs_low, obstacles, slow_c
                     k1 = k * nzs / nz
                     k2 = (k + 1) * nzs / nz
 
-                    print(i,j,k)
-                    if sum(sum(sum(obstacles[ i1:i2 ][ j1:j2 ][ k1:k2 ]))) > 0:
+                    if sum(sum(sum(obstacles[ i1:i2, j1:j2 , k1:k2 ]))) > 0:
                         obstacle[i][j][k] = 1
 
 
         # Add outer domain bounday to obstacle
         for i in range(nx):
             for j in range(ny):
-                obstacle[i][j][1] = 1
+                obstacle[i,j,0] = 1
                 if (j < ny / 2):
-                    obstacle[i-1][j-1][nz_low-1] = 1
+                    obstacle[i, j, nz_low-1] = 1
                 else:
-                    obstacle[i-1][j-1][nz-1] = 1
+                    obstacle[i, j, nz-1] = 1
 
         for i in range(nx):
             for k in range(nz):
-                obstacle[i][0][k] = 1
-                obstacle[i][ny-1][k] = 1
+                obstacle[i,0,k] = 1
+                obstacle[i,ny-1,k] = 1
 
         for k in range(nz):
             for j in range(ny):
-                obstacle[0][j][k] = 1
-                obstacle[nx-1][j][k] = 1
+                obstacle[0,j,k] = 1
+                obstacle[nx-1,j,k] = 1
 
-            obstacle[end_point[0]][end_point[1]][end_point[2]] = -1; # identify end point
+        scl = 2 ** (max(n_vec_exponents)-i_n)
+        end_point_scl = end_point / scl
+        obstacle[int(end_point_scl[0]),int(end_point_scl[1]),int(end_point_scl[2])] = -1 # identify end point
 
-        if (nx == 8):
+        if (nx == 4):
+            obstacle_n4 = obstacle
+        elif (nx == 8):
             obstacle_n8 = obstacle
         elif(nx == 16):
             obstacle_n16 = obstacle
@@ -127,67 +135,72 @@ def laplacian( start_point, end_point, nxs, nys, nzs, nzs_low, obstacles, slow_c
 
     # Initialize solution on first grid
     n_old = n_vec[0]
-    v_old = v0 * np.ones([n_old, n_old / ny_factor, n_old / nz_factor])
+    v_old = v0 * np.ones([int(n_old), int(n_old / ny_factor), int(n_old / nz_factor)])
 
     for i_n in np.arange(1, len(n_vec)):
+
         nx = n_vec[i_n]
         ny = nx / ny_factor
         nz = nx / nz_factor
         nz_low = nz * (nzs_low / nzs)
 
-        if(nx==8):
-          obstacle = obstacle_n8
+
+
+        if(nx==4):
+            obstacle = obstacle_n4
+        elif(nx==8):
+            obstacle = obstacle_n8
         elif(nx==16):
-          obstacle = obstacle_n16
+            obstacle = obstacle_n16
         elif(nx==32):
-          obstacle = obstacle_n32
+            obstacle = obstacle_n32
         elif(nx==64):
-          obstacle = obstacle_n64
+            obstacle = obstacle_n64
         elif(nx==128):
-          obstacle = obstacle_n128
+            obstacle = obstacle_n128
 
-    # Initialize new solution with interpolated old solution of previous grid size
-    v = v0 * np.ones(obstacle.shape) # Gives correct v on obstacles, v_end overwritten later
-    if (nx > n_old):
-        for i in np.arange(1,nx-1):
-            for j in np.arange(1,ny-1):
-                for k in np.arange(1,nz-1):
-                    if (obstacle[i][j][k] == 0): # interior points
-                        i_ = min(nx - 1, max(2, np.floor(i / 2)))
-                        j_ = min(ny - 1, max(2, np.floor(j / 2)))
-                        k_ = min(nz - 1, max(2, np.floor(k / 2)))
-                        v[i][j][k] = v_old[i_][j_][k_]  # update to intepolate later
-    elif(nx < n_old):
-        for i in np.arange(1, nx - 1):
-            for j in np.arange(1, ny - 1):
-                for k in np.arange(1, nz - 1):
-                    if (obstacle[i][j][k] == 0):  # interior points
-                        i_ = min(nx - 1, max(2, np.floor(i * 2)));
-                        j_ = min(ny - 1, max(2, np.floor(j * 2)));
-                        k_ = min(nz - 1, max(2, np.floor(k * 2)));
-                        v[i][j][k] = v_old[i_][j_][k_]  # update to average later
+        # Initialize new solution with interpolated old solution of previous grid size
+        v = v0 * np.ones(obstacle.shape) # Gives correct v on obstacles, v_end overwritten later
+        if (nx > n_old):
+            for i in np.arange(1,int(nx)):
+                for j in np.arange(1,int(ny)):
+                    for k in np.arange(1,int(nz)):
+                        if (obstacle[i,j,k] == 0): # interior points
+                            i_ = min(nx - 1, max(1, int(np.floor(i / 2))))      # CHK1
+                            j_ = min(ny - 1, max(1, int(np.floor(j / 2))))
+                            k_ = min(nz - 1, max(1, int(np.floor(k / 2))))
+                            v[i,j,k] = v_old[i_,j_,k_]  # update to intepolate later
+        elif(nx < n_old):
+            for i in np.arange(1, nx - 1):
+                for j in np.arange(1, ny - 1):
+                    for k in np.arange(1, nz - 1):
+                        if (obstacle[i,j,k] == 0):  # interior points
+                            i_ = min(nx - 1, max(1, int(np.floor(i * 2))))      # CHK2
+                            j_ = min(ny - 1, max(1, int(np.floor(j * 2))))
+                            k_ = min(nz - 1, max(1, int(np.floor(k * 2))))
+                            v[i,j,k] = v_old[i_,j_,k_]  # update to average later
 
-    else:
-        v = v_old
+        else:
+            v = v_old
 
-    # Compute solution, v(i,j,k)
-    # Iterate to get Laplacian solution for potential
+        # Compute solution, v(i,j,k)
+        # Iterate to get Laplacian solution for potential
 
-    for iter in range(iter_max):
-        for i in np.arange(1, nx - 1):
-            for j in np.arange(1, ny - 1):
-                for k in np.arange(1, nz - 1):
-                    if (obstacle[i][j][k] == 1):
-                        v[i][j][k] = v0
-                    elif (obstacle[i][j][k] == -1):
-                        v[i][j][k] = v_end
-                    else:
-                        v[i][k][k] = ( v[i-1][j][k] + v[i+1][j][k] +
-                                       v[i][j-1][k] + v[i][j+1][k] +
-                                       v[i][j][k-1] + v[i][j][k+1] ) / 6
+        for iter in range(iter_max):
+            for i in np.arange(1, nx - 1):
+                for j in np.arange(1, ny - 1):
+                    for k in np.arange(1, nz - 1):
+                        if (obstacle[i,j,k] == 1):
+                            v[i,j,k] = v0
+                        elif (obstacle[i,j,k] == -1):
+                            v[i,j,k] = v_end
+                        else:
+                            v[i,k,k] = ( v[i-1,j,k] + v[i+1,j,k] +
+                                           v[i,j-1,k] + v[i,j+1,k] +
+                                           v[i,j,k-1] + v[i,j,k+1] ) / 6
 
-    v_old = v
-    n_old = nx
+        v_old = v
+        n_old = nx
 
     # ---------------- Convergence Test ---------------
 
@@ -198,18 +211,18 @@ def laplacian( start_point, end_point, nxs, nys, nzs, nzs_low, obstacles, slow_c
         for i in np.arange(1,nx-1):
             for j in np.arange(1,ny-1):
                 for k in np.arange(1,nz-1):
-                    if (obstacle[i][j][k] == 0):
-                        v_min = min( v[i - 1][j][k] + v[i + 1][j][k] +
-                                     v[i][j - 1][k] + v[i][j + 1][k] +
-                                     v[i][j][k - 1] + v[i][j][k + 1] )
-                        v_max = max( v[i - 1][j][k] + v[i + 1][j][k] +
-                                     v[i][j - 1][k] + v[i][j + 1][k] +
-                                     v[i][j][k - 1] + v[i][j][k + 1] )
+                    if (obstacle[i,j,k] == 0):
+                        v_min = min( v[i - 1,j,k] + v[i + 1,j,k] +
+                                     v[i,j - 1,k] + v[i,j + 1,k] +
+                                     v[i,j,k - 1] + v[i,j,k + 1] )
+                        v_max = max( v[i - 1,j,k] + v[i + 1,j,k] +
+                                     v[i,j - 1,k] + v[i,j + 1,k] +
+                                     v[i,j,k - 1] + v[i,j,k + 1] )
 
-                        if v[i][j][k] <= v_min:
+                        if v[i,j,k] <= v_min:
                             not_converged = 1
                             ijk_local_min = [i, j, k]
-                        elif v[i][j][k] >= v_max:
+                        elif v[i,j,k] >= v_max:
                             not_converged = 1
                             ijk_local_max = [i, j, k]
 
@@ -219,28 +232,28 @@ def laplacian( start_point, end_point, nxs, nys, nzs, nzs_low, obstacles, slow_c
 
 
     path = np.NaN * np.ones([nt, 3])
-    path[1] = start_point
+    path[0] = start_point
 
-    for it in np.arange(1, nt+1):
-        i = max(2, min(nx - 1, np.floor(path(1, it - 1))))
-        j = max(2, min(ny - 1, np.floor(path(2, it - 1))))
-        k = max(2, min(nz - 1, np.floor(path(3, it - 1))))
+    for it in np.arange(1, nt):
+        i = max(2, min(nx - 1, np.int(path[it - 1, 0])))
+        j = max(2, min(ny - 1, np.int(path[it - 1, 1])))
+        k = max(2, min(nz - 1, np.int(path[it - 1, 2])))
 
         dv_dx = (
-                    sum( sum( v[i + 1][j - 1:j + 1][k - 1:k + 1] ) ) / 9
-                  - sum( sum( v[i - 1][j - 1:j + 1][k - 1:k + 1] ) ) / 9
+                    sum( sum( v[i + 1,j - 1:j + 1,k - 1:k + 1] ) ) / 9
+                  - sum( sum( v[i - 1,j - 1:j + 1,k - 1:k + 1] ) ) / 9
                 ) /2
         dv_dy = (
-                    sum( sum( v[i - 1:i + 1][j + 1][k - 1:k + 1] ) ) / 9
-                  - sum( sum( v[i - 1:i + 1][j - 1][k - 1:k + 1] ) ) / 9
+                    sum( sum( v[i - 1:i + 1,j + 1,k - 1:k + 1] ) ) / 9
+                  - sum( sum( v[i - 1:i + 1,j - 1,k - 1:k + 1] ) ) / 9
                 ) /2
         dv_dz = (
-                    sum( sum( v[i - 1:i + 1][j - 1:j + 1][k + 1] ) ) / 9
-                  - sum( sum( v[i - 1:i + 1][j - 1:j + 1][k - 1] ) ) / 9
+                    sum( sum( v[i - 1:i + 1,j - 1:j + 1,k + 1] ) ) / 9
+                  - sum( sum( v[i - 1:i + 1,j - 1:j + 1,k - 1] ) ) / 9
                 ) /2
         gradient_vec = np.array([dv_dx, dv_dy, dv_dz])
         gradient_vec = gradient_vec[:,None]
-        path[it] = path[it - 1] + dt * gradient_sign * gradient_vec / (1e-306 + np.linalg.norm(gradient_vec))
+        path[it] = path[it - 1] + dt * gradient_sign * np.squeeze(gradient_vec) / (1e-306 + np.linalg.norm(gradient_vec))
 
     return path, not_converged, nx, ny, nz, nz_low, v
 
